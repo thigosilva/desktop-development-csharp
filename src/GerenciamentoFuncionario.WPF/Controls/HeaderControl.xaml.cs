f018719c2a84349922cafeab88d964c2107de9f5
Gilvan Durães Galindo <gilvan.dgalindo@sp.senac.br>
2022-08-24 12:57:17 -0300

    subir a estrutura do WPF

---
+++ src/GerenciamentoFuncionario.WPF/Controls/HeaderControl.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace GerenciamentoFuncionario.WPF.Controls
+{
+    public sealed partial class HeaderControl : UserControl
+    {
+        public HeaderControl()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
